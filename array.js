82a9262e519ce6ed2fc4ef292e0b7053a860f303
phamthanh47 <54268953+phamthanh47@users.noreply.github.com>
2019-11-08 11:17:01 +0700

    commit

--- array.js
+++ array.js
@@ -2,54 +2,40 @@
 /**
  * Array
  */
-
 // declare an array
-
-const list: number[] = [1, 2, 3];
-
-const categories: Array<string> =
-  ['Sport', 'IT', 'Car'];
-
+var list = [1, 2, 3];
+var categories = ['Sport', 'IT', 'Car'];
 console.log('list');
-list.forEach((num) =>
-  console.log(num.toFixed(2))
-);
-
+list.forEach(function (num) {
+    return console.log(num.toFixed(2));
+});
 console.log('categries');
-categories.forEach((str) =>
-  console.log(str.indexOf('a'))
-);
-
+categories.forEach(function (str) {
+    return console.log(str.indexOf('a'));
+});
 // convert mảng từ dạng này sang dạng khác.
-const listSquare = list.map(num => num * num);
-console.log(listSquare)
+var listSquare = list.map(function (num) { return num * num; });
+console.log(listSquare);
 // Output: [1, 4, 9]
-
 // lọc các phần tử thỏa mãn
-const result = categories.filter(str => str.length > 2)
+var result = categories.filter(function (str) { return str.length > 2; });
 console.log(result);
 // Output: ['Sport', 'Car']
-
-
 /**
  * Tuple
  */
-
 // Declare a tuple type
-let x: [string, number];
+var x;
 // Initialize it
 x = ["hello", 10]; // OK
 // Initialize it incorrectly
 // x = [10, "hello"]; // Error
-
 console.log(x[0].substr(1)); // OK
 // console.log(x[1].substr(1));
 // Error, Property 'substr' does not exist on type 'number'.
-
 x[0] = "world";
 // OK, 'string' can be assigned to 'string | number'
 x[1] = 100.25;
 console.log(x[1].toString());
 // OK, 'string' and 'number' both have 'toString'
-
 // x[6] = true; // Error, 'boolean' isn't 'string | number'
